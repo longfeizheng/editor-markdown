dfe3e84cbbd711d457d86d000ba5a97f3c020bb5
longfeizheng <longfei_zheng@163.com>
2018-03-04 15:50:29 +0800

    添加db.sql

---
+++ db/db.sql
@@ -0,0 +1,31 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 50519
+Source Host           : localhost:3306
+Source Database       : editor-markdown
+
+Target Server Type    : MYSQL
+Target Server Version : 50519
+File Encoding         : 65001
+
+Date: 2018-03-04 15:49:25
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for editor
+-- ----------------------------
+DROP TABLE IF EXISTS `editor`;
+CREATE TABLE `editor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` longtext,
+  `create_time` date DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of editor
+-- ----------------------------
